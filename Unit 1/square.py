ce8a2a21a03e36fdb541813246da8e444095835d
Álvaro Zambrana Fernández <56718986+AlvaroZ-F@users.noreply.github.com>
2020-01-10 10:16:27 +0100

    Update square.py

--- Unit 1/square.py
+++ Unit 1/square.py
@@ -1,10 +1,9 @@
 def square(s):
     area = s*s
     perimeter = s*4
-    return [area, perimeter]
+    return "Square's Area is: "+str(area)+" and its perimeter is: "+str(perimeter)
 
 n = int(input("Introduce the size of the square's side"))
 
-while n > 0:
-    print(square(n))
-    --n
+for i in range(1,n+1):
+    print(square(i))
