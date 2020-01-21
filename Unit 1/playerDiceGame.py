32059f45f401429c31b982fb52b3b3b17803aaed
Álvaro Zambrana Fernández <56718986+AlvaroZ-F@users.noreply.github.com>
2020-01-21 08:58:14 +0100

    Update playerDiceGame.py

--- Unit 1/playerDiceGame.py
+++ Unit 1/playerDiceGame.py
@@ -22,21 +22,30 @@ def checkResult(result):
 def randomDice():
     return random.randint(1,6) #Generates a dice throw result (1 - 6)
 
-def throwPlayer(nplayers): #Number of rounds can be edited but by default will be 2
+def throwPlayer(nplayers): #Full round results
     result = []
-        for j in range(nplayers):
-            a = randomDice() #Generates 3 dice throws and saves them in variables
-            b = randomDice()
-            result.append((a,b)) #Adds those three results as a list, within the result list
+    for j in range(1, nplayers+1):
+        a = randomDice() #Generates 2 dice throws and saves them in variables
+        b = randomDice()
+        result.append(["Player"+str(j),(a,b)]) #Adds those three results as a list, within the result list
     return result
+
+def assembleRounds(nplayers, nrounds):
+    dic_results = {}
+    for i in range(1, nrounds+1):
+        roundname = "Round"+str(i)
+        roundresults = throwPlayer(nplayers)
+        dic_results.update({ roundname: roundresults })
+    return dic_results
 
 numPlayers = int(input("Introduce a number of players"))
 numRounds = int(input("Introduce the number of rounds (If you introduce 0, then by default there'd be 2 rounds): "))
 if numRounds != 0:
-    finalResults = throwPlayer(numPlayers, numRounds)
+    finalResults = assembleRounds(numPlayers, numRounds)
 else:
     finalResults = throwPlayer(numPlayers)
 print("The players results are: ")
 print(finalResults)
-print("The remaining combinations possible are: ")
-print(checkResult(finalResults))
+#print("The remaining combinations possible are: ")
+#print(checkResult(finalResults))
+
