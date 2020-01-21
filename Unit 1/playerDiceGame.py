250b79140133bdf983bae43a3d0837f8dc64e7bd
Álvaro Zambrana Fernández <56718986+AlvaroZ-F@users.noreply.github.com>
2020-01-21 09:30:52 +0100

    Update playerDiceGame.py

--- Unit 1/playerDiceGame.py
+++ Unit 1/playerDiceGame.py
@@ -5,22 +5,30 @@
 """
 
 import random
+import time
+
+
+numPlayers = int(input("Introduce number of players: "))
+numRounds = int(input("Introduce the number of rounds (If you introduce 0, then by default there'd be 2 rounds): "))
 
 
 def allResults(): #Creates by comprehension all combinations possible for a 3 dices throw
     return [(i,j) for i in range(1,7) for j in range(1,7)]
+
 
 def checkResult(result):
     allRe = allResults()
     for i in result: #Runs through the players results list
         for k in i: #Runs through each round inside the list
             for j in allRe: #Runs through all combinations possible, deleting any coincidences
-                if k == j:
+                if k[1] == j:
                     allRe.remove(j)
     return allRe #Returns the edited combinations list
 
+
 def randomDice():
     return random.randint(1,6) #Generates a dice throw result (1 - 6)
+
 
 def throwPlayer(nplayers): #Full round results
     result = []
@@ -30,22 +38,27 @@ def throwPlayer(nplayers): #Full round results
         result.append(["Player"+str(j),(a,b)]) #Adds those three results as a list, within the result list
     return result
 
+
 def assembleRounds(nplayers, nrounds):
     dic_results = {}
     for i in range(1, nrounds+1):
+        ready = input("Hit Enter to Play the Round")
         roundname = "Round"+str(i)
         roundresults = throwPlayer(nplayers)
         dic_results.update({ roundname: roundresults })
+        print("Results of the Round: ")
+        print(["Round"+str(i),roundresults])
     return dic_results
 
-numPlayers = int(input("Introduce a number of players"))
-numRounds = int(input("Introduce the number of rounds (If you introduce 0, then by default there'd be 2 rounds): "))
+
 if numRounds != 0:
     finalResults = assembleRounds(numPlayers, numRounds)
 else:
     finalResults = throwPlayer(numPlayers)
-print("The players results are: ")
+
+time.sleep(5)
+print("The final results of the game are: ")
 print(finalResults)
-#print("The remaining combinations possible are: ")
-#print(checkResult(finalResults))
+print("The remaining combinations possible are: ")
+print(checkResult(finalResults))
 
